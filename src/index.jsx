ae7bff40e081e74fcf0b7caa8d88d9b8bc8e555c
bpramish71 <31367242-bpramish71@users.noreply.replit.com>
2024-05-12 14:03:23 +0000

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import Navbar from './navbar'
+// import Menu from './menu'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+	
+		<Navbar/>
+		<App />
+		
+	</React.StrictMode>
+)
---
+++ src/menu.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./index";
+import "./App.css";
+export const menu = () =>{
+  return(
+    <div className="menu">
+      <ul>
+        <li>
+          <a href="#">you</a>
+        </li>
+        <li>
+          <a href="#">project</a>
+        </li>
+        <li>
+          <a href="#">liked project</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+export default menu;
