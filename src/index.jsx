1e4a75ce0085d84a3fae63c888474358a4945e17
bpramish71 <31367242-bpramish71@users.noreply.replit.com>
2024-05-12 15:20:24 +0000

    first commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import Navbar from './navbar'
-// import Menu from './menu'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Navbar from './navbar';
+import Menu from './menu';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-	
-		<Navbar/>
-		<App />
-		
-	</React.StrictMode>
-)
+const Root = () => {
+	const [menubtnclick, setMenubtnclick] = useState(false);
+
+	const toggleMenu = () => {
+		setMenubtnclick(prevState => !prevState);
+	};
+
+	return (
+		<React.StrictMode>
+			{menubtnclick && <Menu />}
+			{/* Pass toggleMenu function as a prop to Navbar */}
+			<Navbar toggleMenu={toggleMenu} />
+			<App />
+		</React.StrictMode>
+	);
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Root />);
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -1,50 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index";
 import "./App.css";
 
 const Navbar = () => {
-    
-    return (
-        <nav className="navbar">
-            <div className="other">
-                <ul>
-                    
-                    <li className="meanuicon">
-                        {/* Button to toggle menu visibility */}
-                        <button type="button" className="meanuicon">
-                            <svg aria-hidden="true" height="17" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" className="octicon octicon-three-bars Button-visual">
-                                <path className="meanuicon" d="M1 2.75A.75.75 0 0 1 1.75 2h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 2.75Zm0 5A.75.75 0 0 1 1.75 7h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 7.75ZM1.75 12h12.5a.75.75 0 0 1 0 1.5H1.75a.75.75 0 0 1 0-1.5Z"></path>
-                            </svg>
-                        </button>
-                    </li>
-                </ul>
-                <ul className="web">
-                    <li>webname</li>
-                </ul>
-                <ul>
-                    <input className="search" type="text" placeholder="search" />
-                </ul>
-            </div>
-            <ul className="item">
-                <li>
-                    <a href="#">Home</a>
-                </li>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-                <li>
-                    <a href="#">About</a>
-                </li>
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-                <li>
-                    <a href="#">Services</a>
-                </li>
+  return (
+    <nav className="navbar">
+      <div className="other">
+        <ul>
+          <li className="meanuicon">
+            {/* Button to toggle menu visibility */}
+            <button type="button" className="meanuicon" onClick={toggleMenu}>
+              {/* Use appropriate icon or text */}
+              {isMenuOpen ? var menuopen = true : var menuopen = false}
+              <svg
+                aria-hidden="true"
+                height="17"
+                viewBox="0 0 16 16"
+                version="1.1"
+                width="16"
+                data-view-component="true"
+                className="octicon octicon-three-bars Button-visual"
+              >
+                <path
+                  className="meanuicon"
+                  d="M1 2.75A.75.75 0 0 1 1.75 2h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 2.75Zm0 5A.75.75 0 0 1 1.75 7h12.5a.75.75 0 0 1 0 1.5H1.75A.75.75 0 0 1 1 7.75ZM1.75 12h12.5a.75.75 0 0 1 0 1.5H1.75a.75.75 0 0 1 0-1.5Z"
+                ></path>
+              </svg>
+            </button>
+          </li>
+        </ul>
+        <ul className="web">
+          <li>webname</li>
+        </ul>
+        <ul>
+          <input className="search" type="text" placeholder="search" />
+        </ul>
+      </div>
+      <ul className="item">
+        <li>
+          <a href="#">Home</a>
+        </li>
 
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
+        <li>
+          <a href="#">About</a>
+        </li>
 
-        </nav>
-    );
+        <li>
+          <a href="#">Services</a>
+        </li>
+
+        <li>
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
